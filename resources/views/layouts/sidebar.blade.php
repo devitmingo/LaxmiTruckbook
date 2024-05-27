23cebebdd60b5f303784080709fbce6895fec432
PatelTarachand <ptltarachand@gmail.com>
2024-05-27 18:27:26 +0530

    logo update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,22 +1,22 @@
    <div class="leftside-menu">
 
                 <!-- LOGO -->
-                <a href="index.html" class="logo text-center logo-light">
-                    <span class="logo-lg">
-                        <img src="assets/images/logo.png" alt="" height="16">
+                <a href="{{ route('admin.dashboard') }}" class="logo text-center logo-light">
+                    <span class="logo-lg " style="color:white;" >
+                        <h1>TruckBook</h1>
                     </span>
                     <span class="logo-sm">
-                        <img src="assets/images/logo_sm.png" alt="" height="16">
+                        <img src="{{asset('dashboard/assets/images/logo_sm.png')}}" alt="" height="16">
                     </span>
                 </a>
 
                 <!-- LOGO -->
-                <a href="index.html" class="logo text-center logo-dark">
-                    <span class="logo-lg">
-                        <img src="assets/images/logo-dark.png" alt="" height="16">
+                <a href="{{ route('admin.dashboard') }}" class="logo text-center logo-dark">
+                    <span class="logo-lg " style="color:white;" >
+                        <h1>TruckBook</h1>
                     </span>
                     <span class="logo-sm">
-                        <img src="assets/images/logo_sm_dark.png" alt="" height="16">
+                        <img src="{{asset('dashboard/assets/images/logo_sm_dark.png') }}" alt="" height="16">
                     </span>
                 </a>
 
